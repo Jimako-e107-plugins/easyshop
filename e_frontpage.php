0b0bbf72bc6d75a357aa1ac4f599717a45039f42
Jimmi08 <janamicek@gmail.com>
2022-09-05 14:39:16 +0200

    e_frontpage - fixed fatal error, but this is not working anymore

--- e_frontpage.php
+++ e_frontpage.php
@@ -2,6 +2,8 @@
 
 if (!defined('e107_INIT')) { exit(); }
 
+e107::lan('easyshop', NULL);
+
 @include_once(e_PLUGIN.'easyshop/languages/'.e_LANGUAGE.'.php');
 
 $front_page['easyshop'] = array('page' => $PLUGINS_DIRECTORY.'easyshop/easyshop.php', 'title' => PAGE_NAME);
